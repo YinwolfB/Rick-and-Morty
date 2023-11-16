d081859da9ae2edb6f3c56720e9e315d1a04cc63
YinwolfB <comandoxrp@hotmail.com>
2023-11-16 11:22:55 -0500

    Se establece el endPoint y el valor aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import { useFetch } from './hook/useFetch'
 
 function App() {
 
+  const randomIdLocation = Math.floor(Math.random() * 126) + 1
+  const url = `https://rickandmortyapi.com/api/location/${randomIdLocation}`
+  const [location, getLocation] = useFetch(url)
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+
+  console.log(location)
 
   return (
-    <h2>Rick and Morty</h2>  
+    <h2>Rick and Morty</h2>
   )
 }
 
